3771894663b172342173d736cd5fa081cd72d195
jahidjami <jahidhasanjami23@gmail.com>
2024-12-02 13:44:02 +0600

    add html and css file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.expoerts = {
+    content: [
+
+        "./src/**/*.{html,js,ts,jsx.tsx}",  
+],
+theme: {
+      extend: {
+          colors: {
+             'primary': '#ED017F'
+          }
+        },
+    },
+    plugins: [],
+}
